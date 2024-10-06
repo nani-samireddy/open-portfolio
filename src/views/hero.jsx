17ce47e89597ddcc0c509406719e4fb538dfa05b
bhargav0255 <bhargav.pavan.025@gmail.com>
2024-10-06 22:45:26 +0530

    Modified font sizes for multiple screen ratios and cleared the menu bug

--- src/views/hero.jsx
+++ src/views/hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div id="about">
       <MakeCenter>
-        <h1 className="text-5xl md:text-6xl lg:text-7xl  leading-normal lg:leading-relaxed py-20">
+        <h1 className="text-2xl sm:text-2xl md:text-6xl lg:text-7xl  leading-normal lg:leading-relaxed py-20">
           I’m <span className="font-bold">{data.name}</span> - an enthusiastic
           <span className="font-medium">designer & developer</span> based in
           India.
